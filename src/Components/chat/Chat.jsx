e120d811e6a883890db3c0968f369ccb4332a767
Jamal Google <bassemgamal246@gmail.com>
2024-09-03 00:19:45 +0300

    prepare the ui for chat list and chat container

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,11 +1,95 @@
 import './chat.css'
-
-import React from 'react'
+import EmojiBeaker from 'emoji-picker-react'
+import React, { useState } from 'react'
 
 function Chat() {
+  const [open, setOpen] = useState(false)
+  const [message, setMessage] = useState('')
+
+  const hangleEmoji = (e) => {
+    setMessage((prev) => prev + e.emoji)
+  }
+
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className='texts'>
+            <span>Jamal Google</span>
+            <p>Lorem, ipsum dolor here.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center">
+        <div className="message other">
+          <img src="./avatar.png" alt="" />
+          <div className='message-info'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, sunt.</p>
+            <span>2 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className='message-info'>
+            <p>Lorem ipsum dolor sit amet consectetur hello adipisicing elit. Mollitia, sunt.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message other">
+          <img src="./avatar.png" alt="" />
+          <div className='message-info'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, sunt.</p>
+            <span>2 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className='message-info'>
+            <p>Lorem ipsum dolor sit amet consectetur hello adipisicing elit. Mollitia, sunt.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message other">
+          <img src="./avatar.png" alt="" />
+          <div className='message-info'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, sunt.</p>
+            <span>2 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className='message-info'>
+            <img src="https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur hello adipisicing elit. Mollitia, sunt.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder='Type a message...'
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen(!open)} />
+          <EmojiBeaker className='EmojiBeaks' open={open} onEmojiClick={hangleEmoji} />
+        </div>
+        <button className='sendBtn'>Send <img src="./send.png" alt="" /></button>
+      </div>
+    </div>
   )
 }
 
-export default Chat
+export default Chat;
