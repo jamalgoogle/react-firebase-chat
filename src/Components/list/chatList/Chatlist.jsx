dc0d1e38c736d3bbe743a94e3e471e77218795fa
Jamal Google <bassemgamal246@gmail.com>
2024-09-03 00:17:40 +0300

    first commit

--- src/Components/list/chatList/Chatlist.jsx
+++ src/Components/list/chatList/Chatlist.jsx
@@ -1,8 +1,79 @@
 import React from 'react'
-
+import { useState } from 'react'
+import './chatList.css'
 function Chatlist() {
+
+  const [addMode , setAddMode] = useState(false)
   return (
-    <div>Chatlist</div>
+    <div className='chatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search to start new chat' />
+        </div>
+        <img src={ addMode ?  "./minus.png" : "./plus.png"}
+        alt=""
+        className='add'
+        onClick={()=>setAddMode(!addMode)} 
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Bassem Jamal</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/list/userInfo/Userinfo.jsx
+++ src/Components/list/userInfo/Userinfo.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './userInfo.css'
 
 function Userinfo() {
   return (
-    <div>Userinfo</div>
+    <div className='userInfo'>
+      <div className='user'>
+        <img src="./avatar.png" alt="userImg" />
+        <h2>User name</h2>
+      </div>
+      <div className='icons'>
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
+    </div>
   )
 }
 
